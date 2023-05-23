89f8e3aae2f53f6b5f88ca784b62bdb0e7132a94
mengleang <ngounmengleang18@gmail.com>
2023-05-23 15:05:09 +0700

    init repo

---
+++ settings.py
@@ -0,0 +1,11 @@
+SVC_LDAP = False
+SVC_OXPASSPORT = True
+SVC_OXSHIBBOLETH = True
+SVC_MYSQL = True
+
+PERSISTENCE_TYPE = "sql"
+SQL_DB_DIALECT= "mysql"
+SQL_DB_NAME = "gluu"
+SQL_DB_HOST = "mysql"
+SQL_DB_PORT = 3306
+SQL_DB_USER = "gluu"
